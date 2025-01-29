47c061023b66cb3a6f629f52cd4bcc9a35b1397a
Thanush07RAW <draken0067@gmail.com>
2025-01-30 01:09:45 +0530

    Add files via upload

---
+++ While.java
@@ -0,0 +1,14 @@
+public class While {
+    public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int sum=0;
+int i=0;
+while(i<=100) {
+	sum+=i;
+	i+=2;
+}
+System.out.println("Sum of All Even Numbers Between 1 to 100: "+sum);
+	}
+
+}
+
